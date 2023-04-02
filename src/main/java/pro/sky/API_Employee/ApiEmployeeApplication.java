f5e045efaafdac48b7aa8af5e638b1272d4cf719
Александр <hyperpositron@gmail.com>
2023-04-02 17:00:12 +0200

    1

---
+++ src/main/java/pro/sky/API_Employee/ApiEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.API_Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/API_Employee/ApiEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.API_Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
